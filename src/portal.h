32734e96fc9406e6b1b85125a09863400aac3faa
kambot <kameronmpenn@gmail.om>
2018-11-13 13:37:35 -0500

    redesigned the sewers

--- src/portal.h
+++ src/portal.h
@@ -28,28 +28,28 @@ static void init_portals()
 	portal_links[num_portal_links].a.y = 64 * TILE_HEIGHT;
 
 	portal_links[num_portal_links].b.board_index = get_board_index_by_name("Sewers");
-	portal_links[num_portal_links].b.x = 8 * TILE_WIDTH;
-	portal_links[num_portal_links].b.y = 8 * TILE_HEIGHT;
+	portal_links[num_portal_links].b.x = 11 * TILE_WIDTH;
+	portal_links[num_portal_links].b.y = 14 * TILE_HEIGHT;
 
 	++num_portal_links;
 
 	portal_links[num_portal_links].a.board_index = get_board_index_by_name("Sewers");
-	portal_links[num_portal_links].a.x = 60 * TILE_WIDTH;
-	portal_links[num_portal_links].a.y = 63 * TILE_HEIGHT;
+	portal_links[num_portal_links].a.x = 66 * TILE_WIDTH;
+	portal_links[num_portal_links].a.y = 59 * TILE_HEIGHT;
 
 	portal_links[num_portal_links].b.board_index = get_board_index_by_name("Sewers");
-	portal_links[num_portal_links].b.x = 96 * TILE_WIDTH;
-	portal_links[num_portal_links].b.y = 16 * TILE_HEIGHT;
+	portal_links[num_portal_links].b.x = 95 * TILE_WIDTH;
+	portal_links[num_portal_links].b.y = 14 * TILE_HEIGHT;
 
 	++num_portal_links;
 
 	portal_links[num_portal_links].a.board_index = get_board_index_by_name("Sewers");
-	portal_links[num_portal_links].a.x = 157 * TILE_WIDTH;
-	portal_links[num_portal_links].a.y = 20 * TILE_HEIGHT;
+	portal_links[num_portal_links].a.x = 158 * TILE_WIDTH;
+	portal_links[num_portal_links].a.y = 23 * TILE_HEIGHT;
 
 	portal_links[num_portal_links].b.board_index = get_board_index_by_name("Sewers");
-	portal_links[num_portal_links].b.x = 183 * TILE_WIDTH;
-	portal_links[num_portal_links].b.y = 4 * TILE_HEIGHT;
+	portal_links[num_portal_links].b.x = 176 * TILE_WIDTH;
+	portal_links[num_portal_links].b.y = 10 * TILE_HEIGHT;
 
 	++num_portal_links;
 
